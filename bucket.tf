a0186d5f448fc56bcbcef3e897a501b42f2bcec1
jdrodriguezrui <julianruizr1@gmail.com>
2024-05-10 16:45:43 -0500

    Rename bucket

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "test" {
-  name          = "julian-env0-poc-test"
+  name          = "julian-env0-poc-test-renamed"
   location      = "europe-west1"
   project       = "development-env-ta"
   storage_class = "REGIONAL"
