eca47a4e3538b9e567ec4e8bc382d310b5a5fc8b
jdrodriguezrui <julianruizr1@gmail.com>
2024-05-10 16:01:43 -0500

    Update repo for env0 POC

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,7 @@
-#resource "google_storage_bucket" "test" {
-#  name          = "julian-spacelift-poc-test"
-#location      = "europe-west1"
-#  project       = "development-env-ta"
-#  storage_class = "REGIONAL"
-#  force_destroy = true
-#}
+resource "google_storage_bucket" "test" {
+  name          = "julian-env0-poc-test"
+  location      = "europe-west1"
+  project       = "development-env-ta"
+  storage_class = "REGIONAL"
+  force_destroy = true
+}
--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 terraform {
   required_providers {
-    spacelift = {
-      source = "spacelift-io/spacelift"
-    }
     google = {
       source  = "hashicorp/google"
       version = "~> 5.12.0"
